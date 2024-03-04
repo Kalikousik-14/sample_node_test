15c917fc79698ceb7b317b6ef01be541da1589f9
Kalikousik-14 <t.kalikousik@gmail.com>
2024-03-04 12:38:43 +0000

    initial commit

---
+++ server.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+app.post('/check', (req, res) => {
+    const formData = req.body.formData;
+    console.log(formData);
+    // Save formData to the database
+    res.sendStatus(200);
+  });
+
+app.post('/form-data', (req, res) => {
+  const formData = req.body.formData;
+  console.log(formData);
+  // Save formData to the database
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
